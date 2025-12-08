8f54004deaf7a97671bb3998311849b3160b0538
Alisher Muhammadaliyev <alishermuhammadaliyev2508@gmail.com>
2025-12-08 21:04:45 +0500

    finished

--- protfile/script.js
+++ protfile/script.js
@@ -215,7 +215,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         } else if (button.textContent.includes('Download CV')) {
             // Handle CV download
-            showNotification('CV download will be available soon!', 'info');
+            window.open('https://guts.uz/resume.pdf', '_blank');
         }
     }
 
