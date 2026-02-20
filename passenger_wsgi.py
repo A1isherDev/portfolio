e2cfe94f92bccfda75dc5a40133f2417833c2669
Alisher Muhammadaliyev <A1isherDev>
2026-02-20 15:48:28 +0500

    Fix passenger WSGI venv path

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,16 @@
 import sys
 import os
 
-# Add the project directory to the sys.path
-sys.path.append(os.getcwd())
+# Set the project path
+project_path = os.getcwd()
+sys.path.append(project_path)
+
+# Point to your virtualenv if you are using one
+# Replace 'venv' with your actual virtualenv directory name
+# Generally on Serv00 it is in your home directory
+venv_path = os.path.expanduser('~/venv/lib/python3.12/site-packages') # Check your python version!
+if os.path.exists(venv_path):
+    sys.path.insert(0, venv_path)
 
 # Set the Django settings module
 os.environ['DJANGO_SETTINGS_MODULE'] = "portfolio_project.settings"
